24ef903f81127366bf11245418e47461edef1b71
rajaprhub <98698646+rajaprhub@users.noreply.github.com>
2022-11-09 23:12:34 +0530

    daytwod

---
+++ urbancompanyapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import AllRoute from './Pages/AllRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <AllRoute/>
+    </div>
+  );
+}
+
+export default App;
