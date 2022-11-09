905b06ca89619a4ee7129a471aa6ec68bb0b736f
rajaprhub <98698646+rajaprhub@users.noreply.github.com>
2022-11-09 23:15:48 +0530

    daytw

--- urbancompanyapp/src/Components/Navbar.jsx
+++ urbancompanyapp/src/Components/Navbar.jsx
@@ -1,7 +1,6 @@
 import { Box, Flex,Image,Spacer} from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
-
 export default function Navbar() {
     return (
       <Flex
